7f454f6306d71d74df1ff79b33b7872bd950696b
Can Schwabe <can.schwabe@student.htw-berlin.de>
2023-11-14 12:26:35 +0100

    t

--- BarWho/Program.cs
+++ BarWho/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Can sagt tschüss");
+Console.WriteLine("Can sagt tschüss mit freude");
