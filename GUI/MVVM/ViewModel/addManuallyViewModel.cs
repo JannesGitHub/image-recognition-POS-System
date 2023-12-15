2cd92ad606d72c0c082ec0b5ccde1a72a7806f86
Jannes Kleinau <janneskleinau@gmail.com>
2023-12-15 19:05:30 +0100

    Textfeld Filterfunktion bei addManually hinzugefügt.

--- GUI/MVVM/ViewModel/addManuallyViewModel.cs
+++ GUI/MVVM/ViewModel/addManuallyViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GUI.Services;
+using System.Drawing.Printing;
 
 namespace GUI.MVVM.ViewModel
 {
@@ -17,18 +18,55 @@ namespace GUI.MVVM.ViewModel
 
         public addManuallyViewModel(IaddManuallyService addManuallyService)
         {
+            this.products.CollectionChanged += (s, e) =>
+            {
+                DoFiltering();
+            };
+
             _addManuallyService = addManuallyService;
 
-            products = new ObservableCollection<Product>(LineOfGoods.getdummi().lineOfGoods);
+            products = new ObservableCollection<Product>(LineOfGoods.getdummi().lineOfGoods);    
+
+            DoFiltering(); //Damit das Sortiment sofort angezeigt wird
 
             this.AddCommand = new DelegateCommand((o) => { 
                     if(SelectedProduct != null)
                     addManuallyService.AddArticleManually(SelectedProduct);});
         }
 
-        public ObservableCollection<Product> products { get; set; }
+        private ObservableCollection<Product> products { get; set; } = new ObservableCollection<Product> ();
+
+        public ObservableCollection<Product> FilteredProducts { get; set; } = new ObservableCollection<Product>();
 
 
+        private string filter = "";
+
+        public string Filter
+        {get => filter;
+            set
+            {
+                if(value != filter)
+                {
+                    filter = value;
+                    this.OnPropertyChanged(nameof(Filter));
+                    DoFiltering();
+                }
+            }
+        }
+
+        private void DoFiltering()
+        {
+            this.FilteredProducts.Clear();
+            string? value = this.filter?.ToLower();
+            foreach(var item in products)
+            {
+                if(String.IsNullOrEmpty(Filter) ||
+                   item.Name.ToLower().Contains(value) ||
+                   item.Articlenumber.ToString().Contains(value)){
+                    this.FilteredProducts.Add(item);
+                }
+            }
+        }
 
         public Product SelectedProduct { get; set; }
 
